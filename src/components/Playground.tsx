5b99d57075cbd68b83f9e8129d5dd6ba4afbad7d
fxss5201 <723107234@qq.com>
2025-10-28 18:12:14 +0800

    feat: 收起代码一行可以固定底部

--- src/components/Playground.tsx
+++ src/components/Playground.tsx
@@ -18,7 +18,7 @@ function Playground({ code, type = 'tsx', title, description, children }: Playgr
   }
 
   return (
-    <div className='w-full border border-gray-300 dark:border-gray-600 rounded-md overflow-hidden'>
+    <div className='w-full border border-gray-300 dark:border-gray-600 rounded-md'>
       <div className='p-4'>
         {children}
       </div>
@@ -31,7 +31,7 @@ function Playground({ code, type = 'tsx', title, description, children }: Playgr
       </div>
       <Activity mode={codeShow ? 'visible' : 'hidden'}>
         <MdRender md={`\`\`\` ${type}\n${code}\n\`\`\``} className='border-t border-gray-300 dark:border-gray-600' />
-        <div className='h-10 flex items-center justify-center border-t border-gray-300 dark:border-gray-600'>
+        <div className='h-10 flex items-center justify-center border-t border-gray-300 dark:border-gray-600 sticky left-0 right-0 bottom-0 z-10 bg-white dark:bg-gray-800 rounded-b-md'>
           <Button type='text' onClick={toggleCodeShow} icon={<UpOutlined />}>
             {codeShow ? '收起代码' : '显示代码'}
           </Button>
