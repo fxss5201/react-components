66b4e24cee6513cca50036faccf1e4af1e8fc2f1
fxss5201 <723107234@qq.com>
2025-11-05 15:21:21 +0800

    feat: CodeEditor扩展语言

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,6 +1,9 @@
 import { type TextEditorProps } from 'json-edit-react'
-import CodeMirror from '@uiw/react-codemirror'
+import CodeMirror, { basicSetup } from '@uiw/react-codemirror'
 import { json } from '@codemirror/lang-json'
+import { markdown } from '@codemirror/lang-markdown'
+import { javascript } from '@codemirror/lang-javascript'
+import { html } from '@codemirror/lang-html'
 import { githubLight, githubDark } from '@uiw/codemirror-theme-github'
 import { useTheme } from '../storeHooks/useTheme'
 
@@ -13,7 +16,7 @@ function CodeEditor({ value, onChange, onKeyDown }: CodeEditorProps) {
       theme={codeTheme}
       value={value}
       width="100%"
-      extensions={[json()]}
+      extensions={[basicSetup(), json(), markdown(), javascript({ jsx: true }), html()]}
       onChange={onChange}
       onKeyDown={onKeyDown}
     />
