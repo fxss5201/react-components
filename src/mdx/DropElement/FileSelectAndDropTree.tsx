a8b641c073f69cf51e431c98a708eb054ebc9e6c
fxss5201 <723107234@qq.com>
2025-11-28 17:52:23 +0800

    feat: 增加 文件选择并拖拽 的例子

---
+++ src/mdx/DropElement/FileSelectAndDropTree.tsx
@@ -0,0 +1,33 @@
+import { useState, useContext } from 'react'
+import FilesSelect, { type FileTreeItem } from '@/components/FilesSelect'
+import DropElement from '@/components/DropElement'
+import JsonView from '@/components/JsonView'
+import LocaleContext from '@/context/LocaleContext'
+
+function FileSelectAndDropTree() {
+  const localeContext = useContext(LocaleContext)
+  const [listData, setListData] = useState<FileTreeItem[]>([])
+
+  function onSelect(fileList: FileTreeItem[]) {
+    setListData(fileList)
+  }
+
+  return (
+    <>
+      <DropElement targetType='tree' onDrop={onSelect} className='h-30!'>
+        <div className='flex align-center'>
+          <div className='text-2xl'>{
+            localeContext === 'zh'
+              ? '请拖放文件或文件夹到这里，或点击选择文件'
+              : 'Please drag and drop files or folders here, or click to select files'}</div>
+          <FilesSelect targetType='tree' onSelect={onSelect} className='ml-4'></FilesSelect>
+        </div>
+      </DropElement>
+      <JsonView className='mt-4!' jsonClassName='max-h-100 overflow-auto'>
+        {listData}
+      </JsonView>
+    </>
+  )
+}
+
+export default FileSelectAndDropTree
