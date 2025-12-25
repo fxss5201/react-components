290f091198ba8e06769defcb404beb72ea7b7b46
fxss5201 <723107234@qq.com>
2025-12-25 16:02:06 +0800

    feat: 更新 eslint 配置

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,10 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import { defineConfig } from 'eslint/config'
 import { globalIgnores } from 'eslint/config'
 
-export default tseslint.config([
+export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
