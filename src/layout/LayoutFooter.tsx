bb562905beca45db505726fa9e8c424aebe38616
fxss5201 <723107234@qq.com>
2026-01-07 11:58:45 +0800

    feat: 在 LayoutFooter 采用 tailwind-config 中的 @theme inline 适配 ant design 的主题色

--- src/layout/LayoutFooter.tsx
+++ src/layout/LayoutFooter.tsx
@@ -2,23 +2,23 @@ import cn from 'classnames'
 import config from '@/config'
 import { useTranslation } from 'react-i18next'
 import ALink from '@/components/ALink'
-import { useAClassStyles } from '@/Hooks/useStyles'
+// import { useAClassStyles } from '@/Hooks/useStyles'
 
 function LayoutFooter({ className }: { className?: string }) {
   const { t } = useTranslation()
-  const { styles: aClassStyles } = useAClassStyles()
+  // const { styles: aClassStyles } = useAClassStyles()
 
   return (
     <div className={cn('flex justify-between items-start', className)}>
       {config.footer?.links?.length && <div className='flex flex-wrap flex-auto mr-10'>
         {config.footer.links.map(item => (
-          <ALink key={item.key} href={item.link} className={cn('mr-4', aClassStyles.aClass)}>
+          <ALink key={item.key} href={item.link} className={cn('mr-4', 'text-text hover:text-primary')}>
             {t(item.key || item.label, { defaultValue: item.label })}
           </ALink>
         ))}
       </div>}
       {config.footer?.recordInfo && <div className='flex-shrink-0'>
-        <a href={config.footer.recordInfo.recordLink} target='_blank' rel='noreferrer' className={aClassStyles.aClass}>
+        <a href={config.footer.recordInfo.recordLink} target='_blank' rel='noreferrer' className={'text-text hover:text-primary'}>
           {config.footer.recordInfo.recordNumber}
         </a>
       </div>}
