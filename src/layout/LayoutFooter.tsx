c1c899d1a09edc85c244b83522f113f7666a5806
fxss5201 <723107234@qq.com>
2026-01-08 14:45:41 +0800

    feat: 优化 tailwind-config.css

--- src/layout/LayoutFooter.tsx
+++ src/layout/LayoutFooter.tsx
@@ -12,13 +12,13 @@ function LayoutFooter({ className }: { className?: string }) {
     <div className={cn('flex justify-between items-start', className)}>
       {config.footer?.links?.length && <div className='flex flex-wrap flex-auto mr-10'>
         {config.footer.links.map(item => (
-          <ALink key={item.key} href={item.link} className={cn('mr-4', 'text-text hover:text-primary')}>
+          <ALink key={item.key} href={item.link} className={cn('mr-4', 'text-text-color hover:text-primary')}>
             {t(item.key || item.label, { defaultValue: item.label })}
           </ALink>
         ))}
       </div>}
       {config.footer?.recordInfo && <div className='flex-shrink-0'>
-        <a href={config.footer.recordInfo.recordLink} target='_blank' rel='noreferrer' className={'text-text hover:text-primary'}>
+        <a href={config.footer.recordInfo.recordLink} target='_blank' rel='noreferrer' className='a-primary'>
           {config.footer.recordInfo.recordNumber}
         </a>
       </div>}
